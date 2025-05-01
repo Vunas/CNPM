44fe88ea0692d154ae4cf3b30a9ac04b0400c047
Viek0n <156826575+Viek0n@users.noreply.github.com>
2025-05-01 09:43:57 +0700

    Add lại order API

---
+++ frontend/src/api/orderAPI.jsx
@@ -0,0 +1,17 @@
+import axiosClient from "./axiosClient"; // Adjust the path if needed
+
+const orderApi = {
+  getOrders: () => axiosClient.get("/order"),
+  getOrderById: (orderId) => axiosClient.get(`/order/${orderId}`),
+  createOrder: (orderData, orderDetails) =>
+    axiosClient.post("/order", { order: orderData, orderDetails }),
+  updateOrder: (orderId, orderData) =>
+    axiosClient.put(`/order/${orderId}`, orderData),
+  deleteOrder: (orderId) => axiosClient.delete(`/order/${orderId}`),
+  getOrdersByCustomer: (customerId) =>
+    axiosClient.get(`/order/customer/${customerId}`),
+  getOrderDetails: (orderId) => axiosClient.get(`/order-detail/order/${orderId}`),
+
+};
+
+export default orderApi;
