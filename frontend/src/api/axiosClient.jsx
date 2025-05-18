28a7b1204216436c16021f5d17875355759fac28
Vunas <vuh265@gmail.com>
2025-05-18 23:35:43 +0700

    deloy front

--- frontend/src/api/axiosClient.jsx
+++ frontend/src/api/axiosClient.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://cnpm-production-6b4c.up.railway.app/api",
   headers: {
     "content-type": "application/json",
   },
