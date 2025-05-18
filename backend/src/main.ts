ca7396fc90e0abb74a66602470d00d717130d276
Vunas <vuh265@gmail.com>
2025-05-19 00:35:09 +0700

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Cấu hình CORS
   app.enableCors({
-    origin: 'http://localhost:5173', // URL của frontend
+    origin: ['http://localhost:5173', 'https://cnpm-mu.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Nếu bạn sử dụng cookie hoặc thông tin xác thực
+    credentials: true,
   });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
